499fa6f12983a046f24ed7eb012a673c0e5adf9f
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-10 17:46:59 +0000

    Add script to analyze gold medals data

---
+++ Chapter1/gold_medals.R
@@ -0,0 +1,8 @@
+# Look at the gold medals data
+gold_medals
+
+# Note the arguments to median()
+args(median)
+
+# Rewrite this function call, following best practices
+median(gold_medals,na.rm=TRUE)
