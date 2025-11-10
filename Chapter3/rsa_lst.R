ec86c81299155fef99aebb80384704cc6a8803cf
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-10 21:25:08 +0000

    Create rsa_lst.R

---
+++ Chapter3/rsa_lst.R
@@ -0,0 +1,9 @@
+# Add capitals, national_parks, & population to a named list
+rsa_lst <- list(
+  capitals = capitals,
+  national_parks = national_parks,
+  population = population
+)
+
+# List the structure of each element of rsa_lst
+ls.str(rsa_lst)
