f04e17b4a4f432ebe2d46453fa43cb76ba5d4401
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-10 17:55:01 +0000

    Add glm function for generalized linear regression

---
+++ Chapter1/glm.R
@@ -0,0 +1,9 @@
+# Run a generalized linear regression 
+glm(
+  # Model no. of visits vs. gender, income, travel
+  n_visits ~ gender + income + travel, 
+  # Use the snake_river_visits dataset
+  data = snake_river_visits, 
+  # Make it a Poisson regression
+  family = poisson
+)
