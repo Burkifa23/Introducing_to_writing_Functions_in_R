1e82bb9b891343342a083b3445670d78468fe5e7
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-10 17:53:58 +0000

    Modify toss_coin function for weighted sampling
    Updated toss_coin function to allow for weighted sampling based on p_head.

--- Chapter1/toss_coin.R
+++ Chapter1/toss_coin.R
@@ -37,3 +37,16 @@ weights <- p_head
 
 # Update so that heads are sampled with prob p_head
 sample(coin_sides, n_flips, replace = TRUE, prob = c(p_head,1-p_head))
+
+
+# Update the function so heads have probability p_head
+toss_coin <- function(n_flips,p_head) {
+  coin_sides <- c("head", "tail")
+  # Define a vector of weights
+  weights <- c(p_head,1-p_head)
+  # Modify the sampling to be weighted
+  sample(coin_sides, n_flips, replace = TRUE, prob=weights)
+}
+
+# Generate 10 coin tosses
+toss_coin(10,0.8)
