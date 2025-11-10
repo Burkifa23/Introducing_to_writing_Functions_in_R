00eb71574666d2c49868903a13ce3934ac884bfc
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-10 18:31:14 +0000

    Enhance calc_harmonic_mean with na.rm parameter
    Added na.rm argument to calc_harmonic_mean function.

--- Chapter2/get_reciprocal.R
+++ Chapter2/get_reciprocal.R
@@ -16,3 +16,11 @@ std_and_poor500 %>%
   group_by(sector) %>% 
   # Summarize, calculating harmonic mean of P/E ratio
   summarize(hmean_pe_ratio = calc_harmonic_mean(pe_ratio))
+
+# Add an na.rm arg with a default, and pass it to mean()
+calc_harmonic_mean <- function(x, na.rm=FALSE) {
+  x %>%
+    get_reciprocal() %>%
+    mean(na.rm=na.rm) %>%
+    get_reciprocal()
+}
