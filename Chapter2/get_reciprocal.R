853872c277b7f4398bd8490ebc4023675cf834f9
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-10 18:34:08 +0000

    Update get_reciprocal.R

--- Chapter2/get_reciprocal.R
+++ Chapter2/get_reciprocal.R
@@ -24,3 +24,9 @@ calc_harmonic_mean <- function(x, na.rm=FALSE) {
     mean(na.rm=na.rm) %>%
     get_reciprocal()
 }
+
+std_and_poor500 %>% 
+  # Group by sector
+  group_by(sector) %>% 
+  # Summarize, calculating harmonic mean of P/E ratio
+  summarize(hmean_pe_ratio = calc_harmonic_mean(pe_ratio, na.rm=TRUE))
