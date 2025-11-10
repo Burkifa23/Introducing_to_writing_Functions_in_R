25c5a19df4d450e682dfd3c219913e1217f62969
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-10 21:08:35 +0000

    Update pipeable_plot.R

--- Chapter3/pipeable_plot.R
+++ Chapter3/pipeable_plot.R
@@ -18,3 +18,17 @@ plt_dist_vs_speed <- cars %>%
 
 # Now the plot object has a value
 plt_dist_vs_speed
+
+pipeable_plot <- function(data, formula) {
+  plot(formula, data)
+  # Add a "formula" attribute to data
+  attr(data, "formula") <- formula
+  invisible(data)
+}
+
+# From previous exercise
+plt_dist_vs_speed <- cars %>% 
+  pipeable_plot(dist ~ speed)
+
+# Examine the structure of the result
+str(plt_dist_vs_speed)
