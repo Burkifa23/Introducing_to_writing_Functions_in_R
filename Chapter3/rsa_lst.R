9baed92274f15eaa2689472e367686ad1252c195
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-10 21:26:25 +0000

    Convert list to an environment

--- Chapter3/rsa_lst.R
+++ Chapter3/rsa_lst.R
@@ -7,3 +7,9 @@ rsa_lst <- list(
 
 # List the structure of each element of rsa_lst
 ls.str(rsa_lst)
+
+# Convert the list to an environment
+rsa_env <- list2env(rsa_lst)
+
+# List the structure of each variable
+ls.str(rsa_env)
