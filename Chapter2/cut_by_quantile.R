50b3d46369f26723144371eb3942ee94e9e723ef
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-10 18:07:28 +0000

    Set default na.rm to FALSE in cut_by_quantile function

--- Chapter2/cut_by_quantile.R
+++ Chapter2/cut_by_quantile.R
@@ -1,15 +1,15 @@
 # Set the default for n to 5
-cut_by_quantile <- function(x, n=5, na.rm, labels, interval_type) {
+# Set the default for na.rm to FALSE
+cut_by_quantile <- function(x, n = 5, na.rm=FALSE, labels, interval_type) {
   probs <- seq(0, 1, length.out = n + 1)
   qtiles <- quantile(x, probs, na.rm = na.rm, names = FALSE)
   right <- switch(interval_type, "(lo, hi]" = TRUE, "[lo, hi)" = FALSE)
   cut(x, qtiles, labels = labels, right = right, include.lowest = TRUE)
 }
 
-# Remove the n argument from the call
+# Remove the na.rm argument from the call
 cut_by_quantile(
   n_visits, 
-  na.rm = FALSE, 
   labels = c("very low", "low", "medium", "high", "very high"),
   interval_type = "(lo, hi]"
 )
