13103651053c13eeaee1d2c7e54fe04f140c4680
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-10 17:50:49 +0000

    Add toss_coin function to simulate coin toss

--- Chapter1/toss_coin.R
+++ Chapter1/toss_coin.R
@@ -2,3 +2,13 @@ coin_sides <- c("head", "tail")
 
 # Sample from coin_sides once
 sample(coin_sides,1)
+
+
+# Your functions, from previous steps
+toss_coin <- function() {
+  coin_sides <- c("head", "tail")
+  sample(coin_sides, 1)
+}
+
+# Call your function
+toss_coin()
