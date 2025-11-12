f848543cb073c5a6190e4d5e02745240307f3907
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-12 12:59:23 +0000

    fortify_with_metric_units()

--- Chapter4/corn.R
+++ Chapter4/corn.R
@@ -12,3 +12,18 @@ corn %>%
       crop = "corn"
     )
   )
+
+# Wrap this code into a function
+fortify_with_metric_units <- function(data, crop) {
+  data %>%
+    dplyr::mutate(
+      farmed_area_ha = acres_to_hectares(farmed_area_acres),
+      yield_kg_per_ha = bushels_per_acre_to_kgs_per_hectare(
+        yield_bushels_per_acre,
+        crop = crop # Pass the function's 'crop' argument here
+      )
+    )
+}
+
+# Try it on the wheat dataset
+fortify_with_metric_units(wheat, crop = "wheat")
