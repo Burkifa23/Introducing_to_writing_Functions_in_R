09b22971bce6d129bd2e2c0c17714a36bd0f9d55
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-12 13:12:27 +0000

    plot_yield_vs_year()

--- Chapter4/corn.R
+++ Chapter4/corn.R
@@ -36,3 +36,14 @@ ggplot(corn, aes(x=year, y=yield_kg_per_ha)) +
   geom_line(aes(group = state)) +
   # Add a smooth trend layer
   geom_smooth()
+
+
+# Wrap this plotting code into a function
+plot_yield_vs_year <- function(data) {
+  ggplot(data, aes(year, yield_kg_per_ha)) +
+    geom_line(aes(group = state)) +
+    geom_smooth()
+}
+
+# Test it on the wheat dataset
+plot_yield_vs_year(wheat)
