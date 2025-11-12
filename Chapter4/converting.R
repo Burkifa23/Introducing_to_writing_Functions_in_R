9db99f9df41aafe5e4deaecb536ae13143b18509
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-12 12:33:26 +0000

    a function to convert bushels/acre to kg/ha

--- Chapter4/converting.R
+++ Chapter4/converting.R
@@ -70,3 +70,14 @@ bushels_to_kgs <- function(bushels, crop) {
     # Convert lbs to kgs
     lbs_to_kgs()
 }
+
+# Write a function to convert bushels/acre to kg/ha
+bushels_per_acre_to_kgs_per_hectare <- function(bushels_per_acre, crop = c("barley","corn","wheat")) {
+  # Match the crop argument
+  crop <- match.arg(crop)
+  bushels_per_acre %>%
+    # Convert bushels to kgs for this crop
+    bushels_to_kgs(crop) %>%
+    # Convert harmonic acres to ha
+    magrittr::multiply_by(harmonic_acres_to_hectares(1))
+}
