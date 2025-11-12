8f2ea8ecf879915dee49968f7f53002220cbf7cf
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-12 16:13:25 +0000

    run gam  of yield vs. smoothed year and census region function

--- Chapter4/corn.R
+++ Chapter4/corn.R
@@ -83,3 +83,11 @@ plot_yield_vs_year_by_region(wheat)
 
 # Run a generalized additive model of yield vs. smoothed year and census region
 mgcv::gam(yield_kg_per_ha ~ s(year) + census_region, data = corn)
+
+# Wrap the model code into a function
+run_gam_yield_vs_year_by_region <- function(data) {
+  mgcv::gam(yield_kg_per_ha ~ s(year) + census_region, data = data)
+}
+
+# Try it on the wheat dataset
+run_gam_yield_vs_year_by_region(wheat)
