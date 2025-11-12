78e0dda601142aba62e7648f92f3cd055d0f46f0
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-12 16:24:52 +0000

    predict_yields()

--- Chapter4/corn.R
+++ Chapter4/corn.R
@@ -106,3 +106,18 @@ pred_yield_kg_per_ha <- predict(corn_model, predict_this, type = "response")
 predict_this %>%
   # Add the prediction as a column of predict_this 
   dplyr::mutate(pred_yield_kg_per_ha = pred_yield_kg_per_ha)
+
+
+# Wrap this prediction code into a function
+predict_yields <- function(model, year) {
+  predict_this <- data.frame(
+    year = 2050,
+    census_region = census_regions
+  ) 
+  pred_yield_kg_per_ha <- predict(model, predict_this, type = "response")
+  predict_this %>%
+    dplyr::mutate(pred_yield_kg_per_ha = pred_yield_kg_per_ha)
+}
+
+# Try it on the wheat dataset
+predict_yields(wheat_model, year=2050)
