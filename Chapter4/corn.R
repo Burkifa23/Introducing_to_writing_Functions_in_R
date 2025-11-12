d54271188d9b240382fa26f3fc0e9d27f5217081
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-12 13:45:36 +0000

    Plot yield vs. year for the corn dataset

--- Chapter4/corn.R
+++ Chapter4/corn.R
@@ -30,6 +30,8 @@ fortify_with_metric_units(wheat, crop = "wheat")
 
 
 
+
+
 # Using corn, plot yield (kg/ha) vs. year
 ggplot(corn, aes(x=year, y=yield_kg_per_ha)) +
   # Add a line layer, grouped by state
@@ -61,3 +63,8 @@ fortify_with_census_region <- function(data) {
 
 # Try it on the wheat dataset
 fortify_with_census_region(wheat)
+
+# Plot yield vs. year for the corn dataset
+plot_yield_vs_year(corn) +
+  # Facet, wrapped by census region
+  facet_wrap(vars(census_region))
