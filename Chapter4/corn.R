93b9867b4df20d79b88e37779174362544e8e001
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-12 13:17:28 +0000

    Inner join the corn dataset to usa_census_regions by state

--- Chapter4/corn.R
+++ Chapter4/corn.R
@@ -47,3 +47,7 @@ plot_yield_vs_year <- function(data) {
 
 # Test it on the wheat dataset
 plot_yield_vs_year(wheat)
+
+# Inner join the corn dataset to usa_census_regions by state
+corn %>%
+  dplyr::inner_join(usa_census_regions, by = "state")
